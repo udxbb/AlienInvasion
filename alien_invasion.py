a634b6195da6842ec21997a3aa331c70e8f60cf4
xbb <12990798xbb@gmail.com>
2019-01-08 20:56:19 +0800

    add ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,24 +1,29 @@
-import sys
-
 import pygame
+
+from settings import Settings
+from ship import Ship
+import game_functions as gf
 
 
 def run_game():
     # 初始化游戏并创建一个屏幕对象
+
+    # 初始化背景设置
     pygame.init()
-    screen = pygame.display.set_mode((1000, 600))
+    # 创建一个名为screen的显示窗口，实参元组(1000, 600)指定窗口尺寸
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+
+    # 创建一艘飞船
+    ship = Ship(ai_settings, screen)
 
     # 开始游戏的主循环
     while True:
-
-        # 监视键盘和鼠标事件
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-
-                # 让最近绘制的屏幕可见
-        pygame.display.flip()
+        # 调用game_functions中函数
+        gf.check_events(ship)
+        ship.update()
+        gf.update_screen(ai_settings, screen, ship)
 
 
 run_game()
---
+++ bullet.py
@@ -0,0 +1,14 @@
+import pygame
+from pygame.sprite import Sprite
+
+
+class Bullet(Sprite):
+    """一个对飞船发射的子弹进行"""
+
+    def __init__(self, ai_settings, screen, ship):
+        """在飞船所处的位置创建一个子弹对象"""
+        super(Bullet, self).__init__()
+        self.screen = screen
+
+        #在(0，0)处创建一个表示子弹的矩形，在设置正确的位置
+        self.rect = pygame.Rect
