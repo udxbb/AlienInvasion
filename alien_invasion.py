50b095a284567d1fbad1c0891d06964c18ee581f
xbb <12990798xbb@gmail.com>
2019-01-15 08:55:49 +0800

    alien added

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from pygame.sprite import Group
 import game_functions as gf
 
 
@@ -17,13 +18,25 @@ def run_game():
 
     # 创建一艘飞船
     ship = Ship(ai_settings, screen)
+    # 创建一个用于管理子弹的编组
+    bullets = Group()
+
+    # 创建一个用于管理外星人的编组
+    aliens = Group()
+
+    # 创建外星人群
+    gf.create_fleet(ai_settings, screen, aliens)
+
+    # 创建一个外星人
+    # alien = Alien(ai_settings, screen)
 
     # 开始游戏的主循环
     while True:
         # 调用game_functions中函数
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 
 run_game()
