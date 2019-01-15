8979f8183e7770f9cf30e02791f7844305071385
xbb <12990798xbb@gmail.com>
2019-01-15 19:33:57 +0800

    add stats

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import pygame
 
 from settings import Settings
+from game_stats import GameStats
 from ship import Ship
 from pygame.sprite import Group
 import game_functions as gf
@@ -16,6 +17,9 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
+    # 创建一个用于存储游戏统计信息的实例
+    stats = GameStats(ai_settings)
+
     # 创建一艘飞船
     ship = Ship(ai_settings, screen)
     # 创建一个用于管理子弹的编组
@@ -25,7 +29,7 @@ def run_game():
     aliens = Group()
 
     # 创建外星人群
-    gf.create_fleet(ai_settings, screen, aliens)
+    gf.create_fleet(ai_settings, screen, ship, aliens)
 
     # 创建一个外星人
     # alien = Alien(ai_settings, screen)
@@ -34,8 +38,12 @@ def run_game():
     while True:
         # 调用game_functions中函数
         gf.check_events(ai_settings, screen, ship, bullets)
-        ship.update()
-        gf.update_bullets(bullets)
+
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+
         gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 
